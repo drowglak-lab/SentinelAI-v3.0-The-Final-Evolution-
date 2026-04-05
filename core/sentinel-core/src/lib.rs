6a2484420d113a5ed36ef7bc15ca65933e274da9
Aleksei <drowglak@gmail.com>
2026-04-05 16:38:28 +0200

    Update lib.rs

--- core/sentinel-core/src/lib.rs
+++ core/sentinel-core/src/lib.rs
@@ -16,21 +16,17 @@ lazy_static::lazy_static! {
 
 #[pyfunction]
 fn load_policies(path: String) -> PyResult<String> {
-    // 1. Читаем файл (если файла нет - падаем с ошибкой, не трогая память)
     let content = fs::read_to_string(&path)
         .map_err(|e| PyErr::new::<pyo3::exceptions::PyIOError, _>(format!("Read error: {}", e)))?;
 
-    // 2. Валидация (Fail-safe): если YAML кривой, возвращаем ошибку Python
     let config: models::PolicyConfig = serde_yaml::from_str(&content)
         .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("YAML Error: {}", e)))?;
     
-    // 3. Обновление (Atomic-like update)
     GLOBAL_STORE.raw_store.clear();
     for policy in config.policies {
         GLOBAL_STORE.raw_store.insert(policy.id.clone(), Arc::new(policy));
     }
 
-    // 4. Пересборка снимка памяти для быстрых вычислений
     GLOBAL_STORE.rebuild_snapshot();
 
     Ok(format!("SUCCESS: {} policies reloaded. Current version: {}", GLOBAL_STORE.raw_store.len(), config.version))
@@ -41,6 +37,7 @@ fn load_policies(path: String) -> PyResult<String> {
 fn fast_evaluate(tool_name: String, context: HashMap<String, models::AttrValue>) -> PyResult<models::EvaluationResult> {
     let start_time = std::time::Instant::now();
     let snapshot = GLOBAL_STORE.snapshot.load_full();
+    
     let engine = engine::EvaluationEngine { snapshot };
     let result = engine.evaluate(&tool_name, &context);
 
@@ -65,6 +62,7 @@ fn fast_evaluate(tool_name: String, context: HashMap<String, models::AttrValue>)
 fn sentinel_core(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<models::Decision>()?;
     m.add_class::<models::ExecutionMode>()?;
+    m.add_class::<models::EvaluationTrace>()?; // <--- ЭКСПОРТИРУЕМ НОВЫЙ КЛАСС
     m.add_class::<models::EvaluationResult>()?;
     m.add_function(wrap_pyfunction!(load_policies, m)?)?;
     m.add_function(wrap_pyfunction!(fast_evaluate, m)?)?;
